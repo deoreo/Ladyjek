5d4ef03a7abc20b28493ae9a8860072f62c0d16f
richie rosal <richie.rosal@gmail.com>
2015-08-06 17:59:56 +0800

    update layout

--- app/src/main/java/ladyjek/twiscode/com/ladyjek/Activity/ActivitySplashScreen.java
+++ app/src/main/java/ladyjek/twiscode/com/ladyjek/Activity/ActivitySplashScreen.java
@@ -51,7 +51,7 @@ public class ActivitySplashScreen extends Activity {
 
                     // After 5 seconds redirect to another intent
 
-                    Intent i=new Intent(getBaseContext(),ActivityRate.class);
+                    Intent i=new Intent(getBaseContext(),ActivityLogin.class);
 
                     startActivity(i);
 
