98d93f140226e7f1c516e3a8b838b896ef3eda14
richie rosal <richie.rosal@gmail.com>
2015-09-21 17:23:18 +0800

    layout forget password

---
+++ app/src/main/java/ladyjek/twiscode/com/ladyjek/Activity/ActivityForgetPassword_1.java
@@ -0,0 +1,65 @@
+package ladyjek.twiscode.com.ladyjek.Activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import ladyjek.twiscode.com.ladyjek.R;
+
+public class ActivityForgetPassword_1 extends FragmentActivity {
+
+    private ImageView btnBack;
+    private TextView btnOpen;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pass_1);
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        finish();
+        super.onBackPressed();  // optional depending on your needs
+    }
+
+
+}
